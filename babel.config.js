2a1c4fe538e610016cb26aacbda10c5b94342745
Kevin Thomas <hamgom95@gmail.com>
2019-02-09 22:53:16 +0100

    fix bug

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
-module.exports = function (api) {
-  api.cache(true);
-
-  const presets = ["module:metro-react-native-babel-preset"];
-  const plugins = ["@babel/plugin-proposal-class-properties"];
-
-  return {
-    presets,
-    plugins
-  };
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [],
 }
